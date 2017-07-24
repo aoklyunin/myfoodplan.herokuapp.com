f125694ffc0ec2008980d1408cd282941ef71980
aoklyunin <aoklyunin@gmail.com>
2017-07-24 14:30:37 +0300

    ьти

--- plan/forms.py
+++ plan/forms.py
@@ -190,11 +190,10 @@ class AddRecipeForm(Form):
     name = CharField(max_length=10000, label="Название",
                      widget=TextInput(attrs={'placeholder': 'Рагу'}))
 
-    tp = ChoiceField(label="Тип")
+    tp = ModelChoiceField(label="Тип", queryset=RecipeType.objects.all())
 
     def __init__(self, *args, **kwargs):
         super(AddRecipeForm, self).__init__(*args, **kwargs)
-        self.fields['tp'].choices = RecipeType.objects.all()
 
 
 # форма для выбора одного изделия с кол-вом
